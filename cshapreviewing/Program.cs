97f2954c860292e1211c66d7ea1d0c412b8f7d86
Rodolfo Mora <rodolfomora97@gmail.com>
2024-07-11 16:03:20 -0600

    herencia

--- cshapreviewing/Program.cs
+++ cshapreviewing/Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿/* //objeto y clase
 Sale sale = new Sale(20);
 var message = sale.GetInfo();
 
@@ -17,5 +17,46 @@ class Sale //objeto
     public string GetInfo() 
     {
         return "El total es " + Total;
+    }
+}*/
+
+
+// herencia
+
+Sale sale = new Sale(20);
+var message = sale.GetInfo();
+
+Console.WriteLine(message);
+
+class Sale //objeto
+{
+    public decimal Total { get; set; }
+    private string _some; //atributo privado _es para decir que es privado
+
+    public Sale(decimal total)   //contructor
+    {
+        Total = total;
+    }
+
+    //protected string GetInfo() protected lo puede hacer padre e hijo por otro lado no
+    public string GetInfo()
+    {
+        return "El total es " + Total;
+    }
+}
+
+class SaleWithTax : Sale  //herencia como en el contrutor padre pide dato hay que agregarlo tambien en el hijo
+{
+
+    public decimal Tax { get; set; }
+
+    public SaleWithTax(decimal total) : base(total)
+    {
+        Total = total;
+    }
+
+    public string GetInfoWithTax()
+    {
+        return "el impuesto es "+ "impuesto " + Tax;
     }
 }
